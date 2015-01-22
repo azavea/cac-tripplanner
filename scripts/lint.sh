6e81225102cec24d73ceb3c03d55913d0a17929c
Derek Dohler <ddohler@azavea.com>
2015-01-22 15:33:52 -0500

    Mark build unstable on linting errors

---
+++ scripts/lint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function mark_unstable {
+    java -jar jenkins-cli.jar -s $JENKINS_URI set-build-result unstable
+}
+
+set -x
+trap 'mark_unstable' ERR
+
+# Python linting
+vagrant ssh app -c "flake8 /opt/app/python --exclude migrations"
+
+# Run JS linting
+vagrant ssh app -c "cd /opt/app/src && npm run gulp-lint"
