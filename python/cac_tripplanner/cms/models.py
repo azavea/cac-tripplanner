acc143b71a348a5055d8ad5b7b060cea925de34f
Chris Brown <cbrown@azavea.com>
2015-01-22 09:37:25 -0500

    Add publish date and article type to Article model

--- python/cac_tripplanner/cms/models.py
+++ python/cac_tripplanner/cms/models.py
@@ -5,14 +5,25 @@ from ckeditor.fields import RichTextField
 
 class Article(models.Model):
 
+    class ArticleTypes(object):
+        community_profile = 'prof'
+        tips_and_tricks = 'tips'
+
+        CHOICES = (
+            (community_profile, 'Community Profile'),
+            (tips_and_tricks, 'Tips and Tricks'),
+        )
+
     title = models.CharField(max_length=80)
     slug = models.SlugField()
     author = models.ForeignKey(User)
     teaser = RichTextField()  # above the fold
     content = RichTextField(null=True, blank=True)  # below the fold
+    publish_date = models.DateTimeField(blank=True, null=True)
     published = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    content_type = models.CharField(max_length=4, choices=ArticleTypes.CHOICES)
 
     def __unicode__(self):
         return self.title
