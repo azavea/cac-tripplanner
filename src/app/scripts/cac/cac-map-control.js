140e6285c6f2d8fe93a457e99ada14892cff6c78
CloudNiner <andrew.fink.11@gmail.com>
2015-01-22 13:59:21 -0500

    Add layer control with two basemaps + stubbed overlays

--- src/app/scripts/cac/cac-map-control.js
+++ src/app/scripts/cac/cac-map-control.js
@@ -7,21 +7,57 @@ CAC.Map.Control = (function ($, L) {
         zoom: 14
     };
     var map = null;
-    var attribution = [
+
+    var overlaysControl = null;
+
+    var basemaps = {};
+    var overlays = {};
+    var stamenTonerAttribution = [
         'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ',
         'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
         'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
         'under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
     ].join('');
+    var stamenAttribution = [
+        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ',
+        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
+        'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
+        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+    ].join('');
 
     function MapControl(options) {
         this.options = $.extend({}, defaults, options);
+        overlaysControl = new CAC.Map.OverlaysControl();
         map = L.map(this.options.id).setView(this.options.center, this.options.zoom);
 
-        L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
-            attribution: attribution
-        }).addTo(map);
+        initializeBasemaps();
+        initializeOverlays();
+        initializeLayerControl();
     }
 
     return MapControl;
+
+    function initializeBasemaps() {
+        basemaps.Streets = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+            attribution: stamenTonerAttribution
+        });
+
+        basemaps.Terrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
+            attribution: stamenAttribution
+        });
+
+        basemaps.Streets.addTo(map);
+    }
+
+    function initializeOverlays() {
+        overlays['Bike Share Locations'] = overlaysControl.bikeShareOverlay();
+        overlays['Bike Parking'] = overlaysControl.bikeParkingOverlay();
+    }
+
+    function initializeLayerControl() {
+        L.control.layers(basemaps, overlays, {
+            position: 'bottomright'
+        }).addTo(map);
+    }
+
 })(jQuery, L);
