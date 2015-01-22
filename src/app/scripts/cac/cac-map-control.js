7a768ccece3c191974f2e96f7049f3e63fb2acf2
CloudNiner <andrew.fink.11@gmail.com>
2015-01-22 09:52:53 -0500

    Replace OSM basemap with Stamen Toner Lite

--- src/app/scripts/cac/cac-map-control.js
+++ src/app/scripts/cac/cac-map-control.js
@@ -7,13 +7,19 @@ CAC.Map.Control = (function ($, L) {
         zoom: 14
     };
     var map = null;
+    var attribution = [
+        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ',
+        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
+        'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
+        'under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    ].join('');
 
     function MapControl(options) {
         this.options = $.extend({}, defaults, options);
         map = L.map(this.options.id).setView(this.options.center, this.options.zoom);
 
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+            attribution: attribution
         }).addTo(map);
     }
 
