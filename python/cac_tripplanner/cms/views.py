a93338470e9e7915ad3f9b871698f3ae0dd1190e
Sharp Hall <shall@azavea.com>
2015-01-26 13:51:26 -0500

    Move views to appropriate apps

--- python/cac_tripplanner/cms/views.py
+++ python/cac_tripplanner/cms/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
-from cms.models import Article
+from .models import Article
+
+
+def home(request):
+
+    # get randomized community profile
+    community_profile = Article.profiles.random()
+
+    # get randomized tips and tricks
+    tips_and_tricks = Article.tips.random()
+
+    context = RequestContext(request,
+                             dict(community_profile=community_profile,
+                                  tips_and_tricks=tips_and_tricks))
+    return render_to_response('home.html', context_instance=context)
 
 
 def community_profile_detail(request, slug):
@@ -14,6 +28,7 @@ def community_profile_detail(request, slug):
     context = RequestContext(request, {'article': community_profile})
     return render_to_response('community-profile-detail.html',
                               context_instance=context)
+
 
 def tips_and_tricks_detail(request, slug):
     """Tips and tricks detail view
--- python/cac_tripplanner/destinations/views.py
+++ python/cac_tripplanner/destinations/views.py
@@ -1,1 +1,65 @@
-# Create your views here.
+import json
+
+from django.views.generic import View
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.gis.geos import GEOSGeometry, MultiPolygon
+
+import requests
+
+from cac_tripplanner.settings import OTP_URL
+from .models import Destination
+
+
+def map(request):
+    return render_to_response('map.html', context_instance=RequestContext(request))
+
+
+class FindReachableDestinations(View):
+    """Class based view for finding destinations of interest within the calculated isochrone"""
+    # TODO: make decisions on acceptable ranges of values that this endpoint will support
+
+    otp_router = 'default'
+    isochrone_url = OTP_URL.format(router=otp_router) + 'isochrone'
+    algorithm = 'accSampling'
+
+    def isochrone(self, lat, lng, mode, date, time, max_travel_time, max_walk_distance):
+        """Make request to Open Trip Planner for isochrone geometry with the provided args
+        Take OTP JSON and convert it to GDAL MultiPolygon and return it"""
+        payload = {
+            'routerId': self.otp_router,
+            'fromPlace': lat + ',' + lng,
+            'mode': (',').join(mode),
+            'date': date,
+            'time': time,
+            'cutoffSec': max_travel_time,
+            'maxWalkDistance': max_walk_distance,
+            'algorithm': self.algorithm
+        }
+        isochrone_response = requests.get(self.isochrone_url, params=payload)
+
+        # Parse and traverse JSON from OTP so that we return only geometries
+        json_poly = json.loads(isochrone_response.content)[0]['geometry']['geometries']
+        polygons = [GEOSGeometry(json.dumps(poly)) for poly in json_poly]
+
+        # Coerce to multipolygon
+        isochrone_multipoly = MultiPolygon(polygons)
+        return isochrone_multipoly
+
+    def get(self, request, *args, **kwargs):
+        """When a GET hits this endpoint, calculate an isochrone and find destinations within it"""
+        params = request.GET
+
+        iso = self.isochrone(
+            lat=params.get('coords[lat]'),
+            lng=params.get('coords[lng]'),
+            mode=params.getlist('mode[]'),
+            date=params.get('date'),
+            time=params.get('time'),
+            max_travel_time=params.get('maxTravelTime'),
+            max_walk_distance=params.get('maxWalkDistance')
+        )
+
+        matched_objects = Destination.objects.filter(point__within=iso, published=True)
+        return HttpResponse('{{"matched": "{0}"}}'.format(matched_objects), 'application/json')
