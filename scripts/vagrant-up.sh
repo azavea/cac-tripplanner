478cfc6379168925df9723b54b947d28c46cbae4
Kathryn Killebrew <kkillebrew@azavea.com>
2015-03-02 11:28:21 -0500

    Build OTP on Jenkins.
    (Fix failing Python test that expects OTP endpoint to be live.)

--- scripts/vagrant-up.sh
+++ scripts/vagrant-up.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-# The otp machine is currently disabled because it takes a long time, the built-in
-# OTP tests apparently don't work very well, and we don't have any other tests that use it yet.
-vagrant up app database --no-provision
-for vm in database app;
+
+vagrant up otp app database --no-provision
+for vm in otp database app;
 do
     vagrant provision ${vm}
 done
