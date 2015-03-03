d55ac1efde18c56adbd1c387638f1b1fdfed7132
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2015-03-03 16:37:06 -0500

    Revert python linter output

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -9,7 +9,7 @@ trap 'mark_unstable' ERR
 
 # Python linting
 # save to file in format jenkins can read: http://stackoverflow.com/a/5426251
-vagrant ssh app -c "flake8 /opt/app/python --exclude migrations | awk -F\: '{printf '\''%s:%s: [%s]%s\n'\'', $1, $2, substr($4,2,4), substr($4,6)}' > /opt/app/python/violations.txt"
+vagrant ssh app -c "flake8 /opt/app/python --exclude migrations"
 
 # Run JS linting
 vagrant ssh app -c "cd /opt/app/src && npm run gulp-lint-jenkins"
