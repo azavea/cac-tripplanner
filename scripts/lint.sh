3d601a6695d4048adf8592f72ec8b5a386856804
Kathryn Killebrew <kkillebrew@azavea.com>
2015-03-03 15:26:51 -0500

    Save python linter results for Jenkins

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,7 +8,8 @@ set -x
 trap 'mark_unstable' ERR
 
 # Python linting
-vagrant ssh app -c "flake8 /opt/app/python --exclude migrations"
+# save to file in format jenkins can read: http://stackoverflow.com/a/5426251
+vagrant ssh app -c "flake8 /opt/app/python --exclude migrations | awk -F\: '{printf '\''%s:%s: [%s]%s\n'\'', $1, $2, substr($4,2,4), substr($4,6)}' > /opt/app/python/violations.txt"
 
 # Run JS linting
 vagrant ssh app -c "cd /opt/app/src && npm run gulp-lint-jenkins"
