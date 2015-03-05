d479d87a0cddf3b5ce2b39aaa930a9656d2de12b
Kenny Shepard <kshepard@gmail.com>
2015-03-05 18:21:49 -0500

    Add wide_image data migration

---
+++ python/cac_tripplanner/destinations/migrations/0010_default_wide_image.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+# Defaults the wide_image to the image
+def default_wide_image(apps, schema_editor):
+    Destination = apps.get_model("destinations", "Destination")
+    for destination in Destination.objects.all():
+        destination.wide_image = destination.image
+        destination.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('destinations', '0009_destination_wide_image'),
+    ]
+
+    operations = [
+        migrations.RunPython(default_wide_image)
+    ]
