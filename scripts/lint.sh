8095891b4f1bb679790a3475331925a53fe7a944
Kathryn Killebrew <kkillebrew@azavea.com>
2015-03-06 12:40:03 -0500

    Correct exclude argument for flake8

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,9 +8,9 @@ set -x
 trap 'mark_unstable' ERR
 
 # Python linting
-vagrant ssh app -c "flake8 /opt/app/python --exclude migrations"
+vagrant ssh app -c "flake8 /opt/app/python --exclude=migrations"
 # run twice to get console output and to write to file
-vagrant ssh app -c "flake8 /opt/app/python --exclude-migrations --output-file=/opt/app/python/violations.txt"
+vagrant ssh app -c "flake8 /opt/app/python --exclude=migrations --output-file=/opt/app/python/violations.txt"
 
 # Run JS linting
 vagrant ssh app -c "cd /opt/app/src && npm run gulp-lint-jenkins"
