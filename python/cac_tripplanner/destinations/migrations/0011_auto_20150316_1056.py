6bea1d8db36b05aa4affe40155501c1a22516d02
CloudNiner <andrew.fink.11@gmail.com>
2015-03-16 11:07:58 -0400

    Closes #82: Fix makemigrations error due to python 2 limitations

---
+++ python/cac_tripplanner/destinations/migrations/0011_auto_20150316_1056.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import destinations.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('destinations', '0010_default_wide_image'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='destination',
+            name='image',
+            field=models.ImageField(help_text=b'The full-size image. Will be displayed at 400x400.', null=True, upload_to=destinations.models.generate_filename),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='destination',
+            name='wide_image',
+            field=models.ImageField(help_text=b'The half-height image. Will be displayed at 400x200.', null=True, upload_to=destinations.models.generate_filename),
+            preserve_default=True,
+        ),
+    ]
