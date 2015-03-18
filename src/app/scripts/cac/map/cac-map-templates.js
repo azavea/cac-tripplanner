cff63fcce061e74b53fdbcf95503bc97f29a4615
CloudNiner <andrew.fink.11@gmail.com>
2015-03-18 16:50:19 -0400

    Add UNSTYLED intermediate destination detail view

--- src/app/scripts/cac/map/cac-map-templates.js
+++ src/app/scripts/cac/map/cac-map-templates.js
@@ -5,6 +5,7 @@ CAC.Map.Templates = (function (Handlebars, Utils) {
         itinerarySummaries: itinerarySummaries,
         addressText: addressText,
         destinationBlock: destinationBlock,
+        destinationDetail: destinationDetail,
         eventPopup: eventPopup
     };
 
@@ -56,6 +57,25 @@ CAC.Map.Templates = (function (Handlebars, Utils) {
                 '<h5>20 minutes away</h5>',
                 '<img src="{{#if d.wide_image}}{{ d.wide_image }}{{^}}http://placehold.it/300x150{{/if}}" width="300px" height="150px" />',
             '</a>'
+        ].join('');
+        var template = Handlebars.compile(source);
+        var html = template({d: destination});
+        return html;
+    }
+
+    function destinationDetail(destination) {
+        var source = [
+            '<div class="block-detail">',
+                '<h3>{{ d.name }}</h3>',
+                '<h5>20 minutes away</h5>',
+                '<img src="{{#if d.wide_image}}{{ d.wide_image }}{{^}}http://placehold.it/300x150{{/if}}" width="300px" height="150px" />',
+                '{{{ d.description }}}',    // the parent element of whatever is put here is a <p> tag
+                '<div class="row">',
+                    // .back and .getdirections are used to select these elements for the click event
+                    '<a class="back col-xs-6">Back</a>',
+                    '<a class="getdirections col-xs-6">Get Directions</a>',
+                '</div>',
+            '</div>'
         ].join('');
         var template = Handlebars.compile(source);
         var html = template({d: destination});
