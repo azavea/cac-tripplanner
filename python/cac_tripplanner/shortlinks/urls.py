f76d2ace3787c97ba2c40e47238451c8f17dab3c
Kathryn Killebrew <kkillebrew@azavea.com>
2015-05-13 10:39:52 -0400

    Handle variable-length shortened URLs

--- python/cac_tripplanner/shortlinks/urls.py
+++ python/cac_tripplanner/shortlinks/urls.py
@@ -5,7 +5,7 @@ from .views import ShortenedLinkRedirectView, ShortenedLinkCreateView
 
 urlpatterns = patterns(
     '',
-    url(r'^(?P<key>[1-9A-Za-z]{22})$', ShortenedLinkRedirectView.as_view(),
+    url(r'^(?P<key>[1-9A-Za-z]{15,30})$', ShortenedLinkRedirectView.as_view(),
         name='dereference-shortened'),
     url(r'^shorten/$', csrf_exempt(ShortenedLinkCreateView.as_view()),
         name='shorten-link')
