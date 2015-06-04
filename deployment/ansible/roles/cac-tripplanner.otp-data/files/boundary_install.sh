ab468c71972192165e8cad806ed9c5be4160c5d5
Kathryn Killebrew <kkillebrew@azavea.com>
2015-06-04 17:56:57 -0400

    Fix boundary install command args

--- deployment/ansible/roles/cac-tripplanner.otp-data/files/boundary_install.sh
+++ deployment/ansible/roles/cac-tripplanner.otp-data/files/boundary_install.sh
@@ -10,4 +10,4 @@ set -o errexit
 cd /opt
 curl -fsS -d '{"token":"$1"}' -H 'Content-Type: application/json' https://meter.boundary.com/setup_meter > setup_meter.sh
 chmod +x setup_meter.sh
-./setup_meter.sh
+./setup_meter.sh -i "$1" -s --enable-server-metrics -t otp,cac
