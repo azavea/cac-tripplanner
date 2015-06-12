413880e6f0a29b468c1e92e8b037f5b4b95d4d56
CloudNiner <andrew.fink.11@gmail.com>
2015-06-12 11:36:15 -0400

    Closes #374: Add map with intinerary line to directions share page

--- src/app/scripts/cac/pages/cac-pages-directions.js
+++ src/app/scripts/cac/pages/cac-pages-directions.js
@@ -1,11 +1,22 @@
 CAC.Pages.Directions = (function ($, _, DirectionsList, Itinerary, Settings, Utils) {
     'use strict';
 
+    var center = [39.95, -75.1667];
+    var zoom = 12;
+
+    // TODO: Remove duplicate from cac-map-control.js
+    var cartodbAttribution = [
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ',
+        '&copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    ].join('');
+
     var defaults = {
         selectors: {
             directionsContainer: '.directions-list'
         }
     };
+
+    var map = null;
 
     function Directions(options) {
         this.options = $.extend({}, defaults, options);
@@ -31,6 +42,8 @@ CAC.Pages.Directions = (function ($, _, DirectionsList, Itinerary, Settings, Uti
             }
         });
 
+        loadMap();
+
         $.ajax({
             url: Settings.routingUrl,
             type: 'GET',
@@ -40,10 +53,63 @@ CAC.Pages.Directions = (function ($, _, DirectionsList, Itinerary, Settings, Uti
             var itineraries = data.plan.itineraries;
             var params = data.requestParameters;
             var itinerary = new Itinerary(itineraries[itineraryIndex], itineraryIndex, params);
+            setMapItinerary(itinerary);
             directionsListControl.setItinerary(itinerary);
         }, function (error) {
             console.log('error: ', error);
         });
+
+        function loadMap() {
+            var mapOptions = {
+                dragging: false,
+                touchZoom: false,
+                scrollWheelZoom: false,
+                doubleClickZoom: false,
+                boxZoom: false,
+                keyboard: false,
+                zoomControl: false
+            };
+            var retina = '';
+            if (window.devicePixelRatio > 1) {
+                retina = '@2x';
+            }
+            map = cartodb.L.map('directions-map', mapOptions).setView(center, zoom);
+            // TODO: Remove duplicate from cac-map-control.js
+            var basemap = cartodb.L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}' + retina + '.png', {
+                attribution: cartodbAttribution
+            });
+            basemap.addTo(map);
+        }
+
+        function setMapItinerary(itinerary) {
+            map.fitBounds(itinerary.geojson.getBounds(), {
+                padding: [30,30]
+            });
+            itinerary.geojson.addTo(map);
+
+            var originIcon = L.AwesomeMarkers.icon({
+                icon: 'home',
+                prefix: 'fa',
+                markerColor: 'purple'
+            });
+
+            var destIcon = L.AwesomeMarkers.icon({
+                icon: 'flag-o',
+                prefix: 'fa',
+                markerColor: 'red'
+            });
+
+            var origin = [itinerary.from.lat, itinerary.from.lon];
+            var destination = [itinerary.to.lat, itinerary.to.lon];
+            var originOptions = {icon: originIcon, title: 'origin' };
+            var originMarker = new cartodb.L.marker(origin, originOptions);
+
+            var destOptions = {icon: destIcon, title: 'destination' };
+            var destinationMarker = new cartodb.L.marker(destination, destOptions);
+
+            originMarker.addTo(map);
+            destinationMarker.addTo(map);
+        }
     };
 
     return Directions;
