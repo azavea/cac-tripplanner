275b7a4103bae8df7b4572476d3d2314f392526b
Klaas Hoekema <khoekema@azavea.com>
2016-06-29 15:19:16 -0400

    Add 'explore' handling to URL router
    Adds methods to UrlRouter to mediate between local storage and URL
    params, like the ones for directions.  Also restructures how they work
    to enable sharing helper functions.

--- src/app/scripts/cac/urlrouting/cac-urlrouting.js
+++ src/app/scripts/cac/urlrouting/cac-urlrouting.js
@@ -9,25 +9,29 @@ CAC.UrlRouting.UrlRouter = (function (_, $, UserPreferences, Utils, Navigo) {
 
     'use strict';
 
-    // UserPreferences fields that can get written and read from the URL without transformation
-    // So NOT including origin and destination, which are coords in URL but objects in storage
-    var PREF_FIELDS = ['originText', 'destinationText', 'method', 'mode', 'arriveBy', 'maxWalk',
-                       'wheelchair', 'bikeTriangle'];
+    // User pref parameters for different views
+    var SHARED_PREFS = ['origin', 'originText', 'method', 'mode', 'maxWalk',
+                         'wheelchair', 'bikeTriangle'];
+    var DIRECTIONS_PREFS = SHARED_PREFS.concat(['destination', 'destinationText', 'arriveBy']);
+    var EXPLORE_PREFS = SHARED_PREFS.concat(['exploreTime']);
 
     var router = null;
 
     function UrlRouter() {
         router = new Navigo('/map');
 
-        router.on(new RegExp('/directions/?\?'), setPrefsFromDirectionsUrl);
+        router.on(new RegExp('/places/?\?'), setExplorePrefsFromUrl);
+        router.on(new RegExp('/directions/?\?'), setDirectionsPrefsFromUrl);
 
         router.resolve();
     }
 
     UrlRouter.prototype.updateUrl = updateUrl;
     UrlRouter.prototype.clearUrl = clearUrl;
+    UrlRouter.prototype.setExplorePrefsFromUrl = setExplorePrefsFromUrl;
+    UrlRouter.prototype.buildExploreUrlFromPrefs = buildExploreUrlFromPrefs;
+    UrlRouter.prototype.setDirectionsPrefsFromUrl = setDirectionsPrefsFromUrl;
     UrlRouter.prototype.buildDirectionsUrlFromPrefs = buildDirectionsUrlFromPrefs;
-    UrlRouter.prototype.setPrefsFromDirectionsUrl = setPrefsFromDirectionsUrl;
 
     return UrlRouter;
 
@@ -42,48 +46,66 @@ CAC.UrlRouting.UrlRouter = (function (_, $, UserPreferences, Utils, Navigo) {
         updateUrl('');
     }
 
-    // Builds a URL to the directions view by reading the values in local storage
-    function buildDirectionsUrlFromPrefs() {
-        var opts = {};
-        var origin = UserPreferences.getPreference('origin');
-        if (origin && origin.feature && origin.feature.geometry) {
-            opts.origin = [origin.feature.geometry.y, origin.feature.geometry.x].join(',');
-        }
-
-        var destination = UserPreferences.getPreference('destination');
-        if (destination && destination.feature && destination.feature.geometry) {
-            opts.destination = [
-                destination.feature.geometry.y,
-                destination.feature.geometry.x
-            ].join(',');
-        }
-
-        _.forEach(PREF_FIELDS, function(field) {
-            opts[field] = UserPreferences.getPreference(field);
-        });
-
-        var url = '/directions?' + Utils.encodeUrlParams(opts);
-        return url;
+    function setExplorePrefsFromUrl() {
+        UserPreferences.setPreference('method', 'explore');
+        setPrefsFromUrl(EXPLORE_PREFS);
     }
 
-    // Parses the URL and saves the directions parameters in local storage
-    function setPrefsFromDirectionsUrl() {
+    function buildExploreUrlFromPrefs() {
+        return '/places?' + buildUrlParamsFromPrefs(EXPLORE_PREFS);
+    }
+
+    function setDirectionsPrefsFromUrl() {
         UserPreferences.setPreference('method', 'directions');
+        setPrefsFromUrl(DIRECTIONS_PREFS);
+    }
+
+    function buildDirectionsUrlFromPrefs() {
+        return '/directions?' + buildUrlParamsFromPrefs(DIRECTIONS_PREFS);
+    }
+
+
+    /* Parses the URL and saves parameter values into local storage
+     *
+     * Field names in the URL must match those in UserPreferences
+     * 'origin' and 'destination' get special handling to convert from coordinates to GeoJSON
+     *
+     * @param {List[String]} fields : The field names to store values from
+     */
+    function setPrefsFromUrl(fields) {
         var params = Utils.getUrlParams();
-        if (params.destination) {
-            var destCoords = _.map(params.destination.split(','), parseFloat);
-            UserPreferences.setPreference('destination',
-                                          makeFeature(destCoords, params.destinationText));
-        }
-        if (params.origin) {
-            var originCoords = _.map(params.origin.split(','), parseFloat);
-            UserPreferences.setPreference('origin', makeFeature(originCoords, params.originText));
-        }
-        _.forEach(PREF_FIELDS, function(field) {
+        _.forEach(fields, function(field) {
+            // Only set values actually given, don't clobber fields that weren't provided
             if (!_.isUndefined(params[field])) {
-                UserPreferences.setPreference(field, params[field]);
+                // Special handling for origin and destination, which are stored as GeoJSON
+                if (field === 'origin' || field === 'destination') {
+                    var coords = _.map(params[field].split(','), parseFloat);
+                    UserPreferences.setPreference(field, makeFeature(coords, params[field + 'Text']));
+                } else {
+                    UserPreferences.setPreference(field, params[field]);
+                }
             }
         });
+    }
+
+    /* Reads values for the given fields from local storage and composes a URL query string
+     * from them.
+     *
+     * Fields with no value given in the URL will be skipped (not overwritten with nothing)
+     */
+    function buildUrlParamsFromPrefs(fields) {
+        var opts = {};
+        _.forEach(fields, function(field) {
+            if (field === 'origin' || field === 'destination') {
+                var location = UserPreferences.getPreference(field);
+                if (location && location.feature && location.feature.geometry) {
+                    opts[field] = [location.feature.geometry.y, location.feature.geometry.x].join(',');
+                }
+            } else {
+                opts[field] = UserPreferences.getPreference(field);
+            }
+        });
+        return Utils.encodeUrlParams(opts);
     }
 
     function makeFeature(coords, name) {
