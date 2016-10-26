d0f9eaa04a01bdab1e000d60eb8661ca52b56c27
Kathryn Killebrew <kkillebrew@azavea.com>
2016-10-26 15:22:49 -0400

    Fix tests in development
    Provide dummy DOM element for map to load in test.

--- src/test/spec/pages/cac-pages-home-test.js
+++ src/test/spec/pages/cac-pages-home-test.js
@@ -3,8 +3,20 @@
     var HomePages;
 
     beforeEach(function(done) {
+        var mapDiv = document.createElement('div');
+        mapDiv.setAttribute('id', 'map');
+        document.body.appendChild(mapDiv);
         HomePages = new CAC.Pages.Home({});
         HomePages.initialize();
+        done();
+    });
+
+    afterEach(function(done) {
+        var mapDiv = document.getElementById('map');
+        if (mapDiv) {
+            document.body.removeChild(mapDiv);
+        }
+        HomePages = null;
         done();
     });
 
