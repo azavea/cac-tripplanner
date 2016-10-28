6e1077343fc089dedfddd2aa9cc1d54ed4ef144b
Klaas Hoekema <khoekema@azavea.com>
2016-10-28 11:54:26 -0400

    Implement redesigned article list and detail
    Revises the article views and templates in the back-end, including wiring
    up the 'learn' tab to be the articles list view.

--- python/cac_tripplanner/cms/views.py
+++ python/cac_tripplanner/cms/views.py
@@ -11,6 +11,11 @@ from destinations.models import Destination
 from cac_tripplanner.settings import FB_APP_ID, HOMEPAGE_RESULTS_LIMIT, DEBUG
 
 
+DEFAULT_CONTEXT = {
+    'debug': DEBUG,
+    'fb_app_id': FB_APP_ID,
+}
+
 def home(request):
 
     # get randomized community profile
@@ -24,54 +29,40 @@ def home(request):
     shuffle(destination_ids)
     destinations = Destination.objects.filter(id__in=destination_ids[:4])
 
-    context = dict(community_profile=community_profile,
+    context = dict(tab='home',
+                   community_profile=community_profile,
                    tips_and_tricks=tips_and_tricks,
                    destinations=destinations,
-                   fb_app_id=FB_APP_ID,
-                   debug=DEBUG)
+                   **DEFAULT_CONTEXT)
     return render(request, 'home.html', context=context)
 
 
 def about_faq(request, slug):
     page = get_object_or_404(AboutFaq.objects.all(), slug=slug)
-    context = {'page': page, 'debug': DEBUG}
+    context = dict(tab='info', page=page, **DEFAULT_CONTEXT)
     return render(request, 'about-faq.html', context=context)
 
+def learn_list(request):
+    articles = Article.objects.published().order_by('-publish_date')
+    context = dict(tab='info', articles=articles, **DEFAULT_CONTEXT)
+    return render(request, 'learn-list.html', context=context)
 
-def community_profile_detail(request, slug):
-    """Profile/Article view
+def learn_detail(request, slug):
+    article = get_object_or_404(Article.objects.published(), slug=slug)
+    more_articles = Article.objects.published().order_by('-publish_date').exclude(pk=article.pk)[:3]
+    context = dict(tab='info', article=article, more_articles=more_articles, **DEFAULT_CONTEXT)
+    return render(request, 'learn-detail.html', context=context)
 
-    :param slug: article slug to lookup profile
-    """
-    community_profile = get_object_or_404(Article.profiles.published(),
-                                          slug=slug)
-    context = {'article': community_profile, 'debug': DEBUG}
-    return render(request, 'community-profile-detail.html', context=context)
-
-
-def tips_and_tricks_detail(request, slug):
-    """Tips and tricks detail view
-
-    :param slug: article slug to lookup tips and tricks
-    """
-    tips_and_tricks = get_object_or_404(Article.tips.published(),
-                                        slug=slug)
-    context = {'article': tips_and_tricks, 'debug': DEBUG}
-    return render(request, 'tips-and-tricks-detail.html', context=context)
 
 class AllArticles(View):
     """ API endpoint for the Articles model """
 
     def serialize_article(self, request, article):
-        if article.content_type == 'prof':
-            relative_url = reverse(community_profile_detail, args=[article.slug])
-        else:
-            relative_url = reverse(tips_and_tricks_detail, args=[article.slug])
         return {
             'wide_image': article.wide_image.url,
             'narrow_image': article.narrow_image.url,
             'title': article.title,
-            'url': request.build_absolute_uri(relative_url)
+            'url': request.build_absolute_uri(reverse(learn_detail, args=[article.slug]))
         }
 
     def get(self, request, *args, **kwargs):
