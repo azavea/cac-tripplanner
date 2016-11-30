06d93ff8ea7fa7f69a018b80799623b80c55873d
Kathryn Killebrew <kkillebrew@azavea.com>
2016-11-30 18:19:02 -0500

    Rework times listings
    Generate from midnight; hide times prior to now if selected day is today.
    Use minutes since midnight for option values.

--- src/app/scripts/cac/control/cac-control-trip-options.js
+++ src/app/scripts/cac/control/cac-control-trip-options.js
@@ -8,20 +8,28 @@ CAC.Control.TripOptions = (function ($, Handlebars, moment, Modal, UserPreferenc
         currentMode: 'WALK',
         selectors: {
             bodyModalClass: 'body-modal body-modal-options',
-            selectedClass: 'selected', // used to mark selection from a list
+            selectedClass: 'selected', // used to mark selected list item
             visibleClass: 'visible',
             listOptions: 'li.modal-list-choice',
             listOptionsClass: 'modal-list-choice',
+            departAt: '#departAt',
+
+            // date/time selectors
             timingOptions: '.modal-options.timing-modal',
             timingFields: '.modal-options-timing-fields',
             timeOptionsId: 'options-timing-time',
             dayOptionsId: 'options-timing-day',
-            departAt: '#departAt',
+            nextQuarterHourClass: 'next-qtr-hour',
+            earlierQuarterHourClass: 'earlier-qtr-hour',
+            currentTimeClass: 'current-time',
+            notTodayClass: 'not-today',
+            firstOption: 'option:first',
 
+            // the two top-level modals
             bikeOptionsModal: '.modal-options.bike-options',
             walkOptionsModal: '.modal-options.walk-options',
 
-            // mapping of menu option classes to the selector for the next modal to open
+            // mapping of menu option classes to the selector for the child modal to open
             bikeMenuOptions: {
                 'modal-list-indego': '.modal-options.bike-share-select',
                 'modal-list-timing': '.modal-options.timing-modal',
@@ -108,45 +116,40 @@ CAC.Control.TripOptions = (function ($, Handlebars, moment, Modal, UserPreferenc
 
             // TODO: also read/set arriveBy selection
 
-            // Attempt to set date/time selection back using what's stored in preferences.
-            // If either or both not found, will default to today/now.
+            // Set date/time selection back using what's stored in preferences.
             var storedDateTime = UserPreferences.getPreference('dateTime');
 
+            var $day = $(options.selectors.timingFields)
+                .find('#' + options.selectors.dayOptionsId);
+
+            var $time = $(options.selectors.timingFields)
+                .find('#' + options.selectors.timeOptionsId);
+
+            var resetToNow = true; // set false when valid date and/or time option found
             if (storedDateTime) {
+                // parse out the date and time portions
                 var dt = moment.unix(storedDateTime);
                 var day = dt.clone().startOf('date');
-                var time = moment();
-                time.hours(dt.hours());
-                time.minutes(dt.minutes());
-                time.startOf('minute'); // drop seconds/milliseconds
-
-                var $day = $(options.selectors.timingFields)
-                    .find('#' + options.selectors.dayOptionsId);
-
-                var $time = $(options.selectors.timingFields)
-                    .find('#' + options.selectors.timeOptionsId);
+                var time = dt.diff(day, 'minutes'); // minutes since midnight on selected day
 
                 // check if value is an available option before selecting it;
                 // if not (more than a week passed?) will revert to default of 'now'
                 if ($day.find('option[value="' + day.unix().toString() + '"]').length > 0) {
                     $day.val(day.unix().toString());
-                } else {
-                    UserPreferences.setPreference('dateTime', undefined);
+                    resetToNow = false;
                 }
 
-                if ($time.find('option[value="' + time.unix().toString() + '"]').length > 0) {
-                    $time.val(time.unix().toString());
-                } else {
-                    // add a day to find times wrapped past midnight
-                    time.add(1, 'day');
-                    if ($time.find('option[value="' + time.unix().toString() + '"]').length > 0) {
-                        $time.val(time.unix().toString());
-                    } else {
-                        UserPreferences.setPreference('dateTime', undefined);
-                        // unset day as well
-                        $day.val($day.find('option:first').val());
-                    }
+                if ($time.find('option[value="' + time + '"]').length > 0) {
+                    $time.val(time);
+                    resetToNow = false;
                 }
+            }
+
+            // default to current date and time
+            if (resetToNow) {
+                UserPreferences.setPreference('dateTime', undefined);
+                $day.val($day.find(options.selectors.firstOption).val());
+                $time.val($time.find('.' + options.selectors.currentTimeClass).val());
             }
         }
 
@@ -185,7 +188,26 @@ CAC.Control.TripOptions = (function ($, Handlebars, moment, Modal, UserPreferenc
                 childModal.close();
             }
 
-        } // else: user clicked somewhere on modal that is not an option; ignore
+        } else {
+            // user clicked somewhere on modal that is not a list item option
+
+            if ($el.prop('id') === options.selectors.dayOptionsId) {
+                if (!$el.val()) {
+                    // set to current day
+
+                    // hide times before 'now'
+                    $(options.selectors.timingFields).removeClass(options.selectors.notTodayClass);
+                    // TODO: set time to 'now' if selection is on or before next 15 minute increment
+
+
+                } else {
+                    // not today; hide 'now' option
+                    $(options.selectors.timingFields).addClass(options.selectors.notTodayClass);
+                    // TODO: set to next 15 minute increment if 'now' was selected
+
+                }
+            }
+        }
     }
 
     function onChildModalClose() {
@@ -209,18 +231,14 @@ CAC.Control.TripOptions = (function ($, Handlebars, moment, Modal, UserPreferenc
         var when;
 
         if (!!selectedTime && !!selectedDay) {
-            selectedDay = moment.unix(selectedDay);
-            selectedTime = moment.unix(selectedTime);
+            when = moment.unix(selectedDay);
 
             // set the time portion on the selected day
-            when = selectedDay.clone();
-            when.hours(selectedTime.hours());
-            when.minutes(selectedTime.minutes());
+            when.add(selectedTime, 'minutes');
 
         } else if (!!selectedTime) {
-            when = moment.unix(selectedTime);
-        } else if (!!selectedDay) {
-            when = moment.unix(selectedDay);
+            // have time but not day; use today
+            when = moment().startOf('date').add(selectedTime, 'minutes');
         } else {
             when = undefined;
         }
@@ -248,16 +266,16 @@ CAC.Control.TripOptions = (function ($, Handlebars, moment, Modal, UserPreferenc
                 '{{/each}}',
                 '</select></li>',
                 '<li><select class="modal-options-timing-select" id="{{timeOptionsId}}">',
-                    '<option value="">Now</option>',
                     '{{#each times}}',
-                        '<option value="{{this.value}}">{{this.label}}</option>',
+                        '<option class={{this.classes}} value="{{this.value}}">{{this.label}}</option>',
                     '{{/each}}',
                 '</select></li>'
         ].join('');
 
-        var time = moment();
-        var day = moment().startOf('date'); // set to 12 AM to normalize
+        var midnight = moment().startOf('date'); // set to 12 AM to normalize
+        var day = midnight.clone();
 
+        // offer options for next 7 days
         var days = [];
         for (var i = 1; i < 8; i++) {
             day = day.add(1, 'days');
@@ -267,20 +285,43 @@ CAC.Control.TripOptions = (function ($, Handlebars, moment, Modal, UserPreferenc
             });
         }
 
+        // generate list of options in quarter hour increments within a 24 hour window
         var times = [];
+        // round current time up to the next minute
+        var now = moment().add(1, 'minute').startOf('minute');
+        var passedNow = false;
 
-        // round first listed time to 15 minutes
-        var MS_TO_15_MIN = 15 * 60 * 1000;
-        time = moment(Math.round((time - (MS_TO_15_MIN / 2)) / MS_TO_15_MIN) * MS_TO_15_MIN);
+        // reset day to midnight
+        day = midnight.clone();
+        var MINUTES_IN_DAY = 24 * 60;
 
-        // generate list of options in 15 minute increments for next 24 hour window
-        var QTR_HRS_IN_DAY = 96;
-        for (var j = 0; j < QTR_HRS_IN_DAY; j++) {
-            time = time.add(15, 'minutes');
+        for (var j = 0; j < MINUTES_IN_DAY; j = j + 15) {
+            var classes = '';
+
+            if (!passedNow) {
+                // note times earlier than now to hide when selected day is today
+                classes = options.selectors.earlierQuarterHourClass;
+
+                passedNow = day > now;
+                if (passedNow) {
+                    // just found the next quarter hour from the current time; note it
+                    classes = options.selectors.nextQuarterHourClass;
+                    // also add an option for the current time
+                    times.push({
+                        value: '',
+                        label: 'Now',
+                        classes: options.selectors.currentTimeClass
+                    });
+                }
+            }
+
             times.push({
-                value: time.unix(),
-                label: time.format('h:mm a')
+                value: j,
+                label: day.format('h:mm a'),
+                classes: classes
             });
+
+            day.add(15, 'minutes');
         }
 
         var template = Handlebars.compile(source);
@@ -302,9 +343,9 @@ CAC.Control.TripOptions = (function ($, Handlebars, moment, Modal, UserPreferenc
         var $day = $timing.find('#' + options.selectors.dayOptionsId);
         var $time = $timing.find('#' + options.selectors.timeOptionsId);
 
-        // set date and time selectors to first option, for today/now
-        $day.val($day.find('option:first').val());
-        $time.val($time.find('option:first').val());
+        // set date and time selectors to today/now
+        $day.val($day.find(options.selectors.firstOption).val());
+        $time.val($time.find('.' + options.selectors.currentTimeClass).val());
 
         // reset to 'depart at'
         $(childModalSelector).find(options.selectors.departAt).click();
@@ -312,6 +353,6 @@ CAC.Control.TripOptions = (function ($, Handlebars, moment, Modal, UserPreferenc
         // un-set user preferences (will revert to defaults)
         UserPreferences.setPreference('arriveBy', undefined);
         UserPreferences.setPreference('dateTime', undefined);
-}
+    }
 
 })(jQuery, Handlebars, moment, CAC.Control.Modal, CAC.User.Preferences);
