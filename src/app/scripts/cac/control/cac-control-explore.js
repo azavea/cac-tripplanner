312a64aeca31a6004f4a0565b15dcfc7fab20eee
Kathryn Killebrew <kkillebrew@azavea.com>
2017-01-16 14:44:58 -0500

    Initialize slider text
    Initialize value in text box next to isochrone slider.

--- src/app/scripts/cac/control/cac-control-explore.js
+++ src/app/scripts/cac/control/cac-control-explore.js
@@ -67,6 +67,10 @@ CAC.Control.Explore = (function (_, $, Geocoder, MapTemplates, HomeTemplates, Ro
 
         // update isochrone on slider move
         $(options.selectors.isochroneSlider).change(setOptions);
+
+        // initialize slider text value
+        $(options.selectors.isochroneOutput).val($(options.selectors.isochroneSlider).val());
+
         showPlacesContent();
     }
 
