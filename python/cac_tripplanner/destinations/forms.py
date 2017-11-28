360af02c40a6cafa04b251ff20480dceaf131c76
Kathryn Killebrew <kkillebrew@azavea.com>
2017-11-28 13:22:16 -0500

    Add event date validation
    Start date must be before end date.

--- python/cac_tripplanner/destinations/forms.py
+++ python/cac_tripplanner/destinations/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 
 from .models import Destination, Event
 from cac_tripplanner.image_utils import validate_image
@@ -24,3 +24,14 @@ class EventForm(DestinationForm):
     class Meta:
         model = Event
         exclude = []
+
+    def clean(self):
+        """Validate start date is less than end date"""
+        cleaned_data = super(EventForm, self).clean()
+        start = self.cleaned_data.get('start_date')
+        end = self.cleaned_data.get('end_date')
+
+        if start and end and start >= end:
+            self.add_error('start_date', ValidationError('Start date must be before end date.'))
+
+        return cleaned_data
