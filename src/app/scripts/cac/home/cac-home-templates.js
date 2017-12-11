2adb8be644f2b69701877ab14eaf519eda5ac6bd
Kathryn Killebrew <kkillebrew@azavea.com>
2017-12-11 16:30:06 -0500

    Move destinations filter template
    Create partial for destinations filter button bar and precompile it.

--- src/app/scripts/cac/home/cac-home-templates.js
+++ src/app/scripts/cac/home/cac-home-templates.js
@@ -1,14 +1,72 @@
 CAC.Home.Templates = (function (Handlebars) {
     'use strict';
 
+    // precompiled HTML snippets
+    var filterButtonBarTemplate = '';
+
     var module = {
-        destinations: destinations
+        destinations: destinations,
+        getFilterButtonBar: getFilterButtonBar
     };
+
+    setupFilterTemplates();
 
     return module;
 
+    function getFilterButtonBar() {
+        return filterButtonBarTemplate;
+    }
+
     /**
-     * Take list of destination objects and return templated HTML snippet for sidebar.
+     * Dynamically select which filter control to use (button bar or dropdown).
+     *
+     * @return {String} name of a registered Handlebars partial
+     */
+    function filterPartial() {
+        // TODO: implement
+        return 'filterButtonBar';
+    }
+
+    // Initialize by registering the two compiled partials for the filter bar,
+    // which be chosen dynamically when building the destinations list.
+    function setupFilterTemplates() {
+        var filterButtonBar = [
+            '<div class="filter-picker">',
+                '<div class="filter-toggle">',
+                    '<div class="all filter-option on" title="All" data-filter="All">',
+                        '<span class="filter-label">All</span>',
+                    '</div>',
+                    '<div class="events filter-option" title ="Events" ',
+                        'data-filter="Events">',
+                        '<span class="filter-label">Events</span>',
+                    '</div>',
+                    '<div class="nature filter-option" title="Nature" ',
+                        'data-filter="Nature">',
+                        '<span class="filter-label">Nature</span>',
+                    '</div>',
+                    '<div class="exercise filter-option" title="Exercise"',
+                        'data-filter="Exercise">',
+                        '<span class="filter-label">Exercise</span>',
+                    '</div>',
+                    '<div class="relax filter-option" title="Relax" data-filter="Relax">',
+                        '<span class="filter-label">Relax</span>',
+                    '</div>',
+                    '<div class="educational filter-option" title ="Educational"',
+                        'data-filter="Educational">',
+                        '<span class="filter-label">Educational</span>',
+                    '</div>',
+                    '<input type="hidden" name="destination-filter" value="All">',
+                '</div>',
+            '</div>'].join('');
+
+        filterButtonBarTemplate = Handlebars.compile(filterButtonBar);
+        Handlebars.registerPartial('filterButtonBar', filterButtonBarTemplate);
+        Handlebars.registerHelper('filterPartial', filterPartial);
+    }
+
+    /**
+     * Take list of destination objects and return templated HTML snippet for places list.
+     * Note this template HTML is also part of the Django template `home.html`.
      *
      * @param useDestinations {Array} Collection of JSON destinations from /api/destinations/search
      * @param alternateMessage {String} Text to display if there are no destinations
@@ -20,33 +78,7 @@ CAC.Home.Templates = (function (Handlebars) {
                 '<div class="places-header-content">',
                     '<h1>Places we love</h1>',
                     '<a href="#" class="map-view-btn">Map View</a>',
-                    '<div class="filter-picker">',
-                        '<div class="filter-toggle">',
-                            '<div class="all filter-option on" title="All" data-filter="All">',
-                                '<span class="filter-label">All</span>',
-                            '</div>',
-                            '<div class="events filter-option" title ="Events" ',
-                                'data-filter="Events">',
-                                '<span class="filter-label">Events</span>',
-                            '</div>',
-                            '<div class="nature filter-option" title="Nature" ',
-                                'data-filter="Nature">',
-                                '<span class="filter-label">Nature</span>',
-                            '</div>',
-                            '<div class="exercise filter-option" title="Exercise"',
-                                'data-filter="Exercise">',
-                                '<span class="filter-label">Exercise</span>',
-                            '</div>',
-                            '<div class="relax filter-option" title="Relax" data-filter="Relax">',
-                                '<span class="filter-label">Relax</span>',
-                            '</div>',
-                            '<div class="educational filter-option" title ="Educational"',
-                                'data-filter="Educational">',
-                                '<span class="filter-label">Educational</span>',
-                            '</div>',
-                            '<input type="hidden" name="destination-filter" value="All">',
-                        '</div>',
-                    '</div>',
+                    '{{> (filterPartial) }}',
                 '</div>',
             '</header>',
             '{{#unless alternateMessage}}',
@@ -86,6 +118,7 @@ CAC.Home.Templates = (function (Handlebars) {
             '</div>',
             '{{/if}}',
         ].join('');
+
         var template = Handlebars.compile(source);
         var html = template({destinations: useDestinations,
                              alternateMessage:alternateMessage},
