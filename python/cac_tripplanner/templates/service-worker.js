f851eb51d1e74dc9e41cbf2781483cfafe83189d
Kathryn Killebrew <kkillebrew@azavea.com>
2018-01-02 12:04:23 -0500

    Bump service worker version
    For browsers with the site cached to pick up on the new styles and JS.

--- python/cac_tripplanner/templates/service-worker.js
+++ python/cac_tripplanner/templates/service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker to support functioning as a PWA
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 
-var CACHE_NAME = 'cac_tripplanner_v3';
+var CACHE_NAME = 'cac_tripplanner_v4';
 
 var cacheFiles = {{ cache_files | safe }};
 
