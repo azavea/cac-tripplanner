1daf57b4b4e185e76cbbe1988aa54975c00408d8
Kathryn Killebrew <kkillebrew@azavea.com>
2018-01-05 14:20:19 -0500

    Conditional directions link from static home page
    When home page rendered via Django template, use custom template tag to get ID of destination to use
    for directions, if any. Hide directions button for events with no destination.

--- python/cac_tripplanner/destinations/templatetags/destination_extras.py
+++ python/cac_tripplanner/destinations/templatetags/destination_extras.py
@@ -6,3 +6,17 @@ register = template.Library()
 def has_activity(destination, activity_name):
     """Test if a given activity is available at the passed destination"""
     return destination.has_activity(activity_name) if destination else False
+
+@register.simple_tag(name='get_directions_id')
+def get_directions_id(destination):
+    """Get place ID for directions, which is place ID for associated destination, if an event"""
+    if hasattr(destination, 'destination'):
+        # event with a related destination; use it for directions
+        if destination.destination:
+            return destination.destination.id
+        else:
+            # event without a destination
+            return None
+    else:
+        # not an event
+        return destination.id
