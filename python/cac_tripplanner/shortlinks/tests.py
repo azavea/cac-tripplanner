8caf115eb7d38d0e80b08d419915666290081d33
Kathryn Killebrew <kkillebrew@azavea.com>
2019-08-29 11:57:46 -0400

    Fix urls imports for 2.x
    Fix imports of urlresolvers to django.urls for Django 2.x upgrade.

--- python/cac_tripplanner/shortlinks/tests.py
+++ python/cac_tripplanner/shortlinks/tests.py
@@ -1,7 +1,7 @@
 import json
 import urllib.parse
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.test import TestCase, Client, override_settings
 from django.utils import timezone
 
