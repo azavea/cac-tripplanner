2bf29e8e7ec5c3c5474575fd3d19bdcfb544be8f
Rachele Morino <rachelekm@gmail.com>
2025-04-07 14:41:22 -0400

    Add script for manual GTFS edits

---
+++ deployment/dataproc/gtfs-feed-fetcher/migrate_feeds_to_otpv1.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+"""Modifies GTFS files to OTP v1 standard.
+
+OTPv2 updates as GTFS standard updates, however not backported to OTPv1.
+We can't migrate to OTPv2, so manually modify GTFS for OTPv1 support level.
+"""
+import logging
+import zipfile
+import os
+
+logging.basicConfig()
+LOG = logging.getLogger()
+LOG.setLevel(logging.INFO)
+
+gtfs_dir = 'gtfs'
+
+def unzip_file(filename):
+    feed_path = gtfs_dir + "/" + filename + ".zip"
+    if zipfile.is_zipfile(feed_path):
+        with zipfile.ZipFile(feed_path, 'r') as zip_ref:
+            zip_ref.extractall(gtfs_dir)
+            LOG.debug('Unzipped %s', filename)
+            return
+    else:
+        LOG.warn('File %s does not look like a valid zip file.', filename)
+
+def clean_up_txt_files():
+    for root, _, files in os.walk(gtfs_dir):
+        for file in files:
+            if file.endswith('.txt'):
+                os.remove(os.path.join(root, file))
+    LOG.debug('Cleaned up .txt files')
+
+def re_zip_files(filename):
+    feed_path = gtfs_dir + "/" + filename + ".zip"
+    with zipfile.ZipFile(feed_path, 'w', zipfile.ZIP_DEFLATED) as zip_ref:
+        for root, _, files in os.walk(gtfs_dir):
+            for file in files:
+                if file.endswith('.txt'):
+                    file_path = os.path.join(root, file)
+                    zip_ref.write(file_path, os.path.relpath(file_path, gtfs_dir))
+        LOG.debug('Re-zipped %s', feed_path)
+
+def modify_septa_bus():
+    # New GTFS includes replacement trolleybuses, unsupported transit type in OTPv1
+    # Convert to buses to prevent error building OTP
+    # Reference: https://github.com/septadev/GTFS?tab=readme-ov-file#routestxt
+    txt_path_to_update = os.path.join(gtfs_dir, 'routes.txt')
+    content=''
+    try:
+        with open(txt_path_to_update, 'r') as file:
+            content = file.read()
+            content = content.replace(',,11,,', ',,3,,')
+        with open(txt_path_to_update, 'w') as file:
+            file.write(content)
+        LOG.debug('Modified %s', txt_path_to_update)
+    except Exception as e:
+        raise ValueError('Error modifying %s: %s', txt_path_to_update, e)
+
+def main():
+    filenames_to_modify = ['septa_bus']
+    for filename in filenames_to_modify:
+        unzip_file(filename)
+        # Run modifications
+        # Namely modify SEPTA bus GTFS for OTPv1 support level
+        modify_septa_bus()
+        # Clean up
+        re_zip_files(filename)
+        clean_up_txt_files()
+    
+    LOG.info('Updating GTFS feeds for OTPv1 support complete')
+
+if __name__ == "__main__":
+    main()
