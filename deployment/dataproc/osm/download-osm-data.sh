c4c33da7835c9913c4867f1f64011567df0717c8
Rachele Morino <rachelekm@gmail.com>
2025-09-03 09:38:05 -0400

    Modify to use latest timestamp for OSM download
    Geofabrik is now encoraging downloading the latest timestamped file download in place of latest tagged pbf file. On downloading `-latest` file for state we'll receive a 302 status code and html response that file has ben moved. Adjusts our osm download script to follow this new convention.

--- deployment/dataproc/osm/download-osm-data.sh
+++ deployment/dataproc/osm/download-osm-data.sh
@@ -3,6 +3,17 @@
 declare -a STATES=("pennsylvania" "new-jersey" "delaware")
 
 for STATE in "${STATES[@]}"; do
+    echo "Fetching latest OSM data timestamp for ${STATE}..."
+    # Fetch the state.txt file to get the latest timestamp and extract date
+    TIMESTAMP=$(curl -s "https://download.geofabrik.de/north-america/us/${STATE}-updates/state.txt" | grep "timestamp=" | awk -F'[=T]' '{print $2}')
+    if [ -z "$TIMESTAMP" ]; then
+        echo "Could not find a timestamp for ${STATE}. Skipping."
+        continue
+    fi
+
+    # Format the timestamp to YYYYMMDD for url
+    FORMATTED_TIMESTAMP=$(echo "$TIMESTAMP" | awk -F'-' '{printf("%s%s%s\n", substr($1,3,2), $2, $3)}')
+
     echo "downloading OSM data for ${STATE}..."
-    curl https://download.geofabrik.de/north-america/us/${STATE}-latest.osm.pbf -o ${STATE}.pbf
+    curl https://download.geofabrik.de/north-america/us/${STATE}-${FORMATTED_TIMESTAMP}.osm.pbf -o ${STATE}.pbf
 done
